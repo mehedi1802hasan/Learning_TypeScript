9ae2d352b22b55ce75f29f3998f73f6756a12a0e
mehedi1802hasan <mehedi1802hasan@gmail.com>
2024-05-03 01:22:08 +0600

    first commit

---
+++ Module-1/m1/src/1.4.ts
@@ -0,0 +1,28 @@
+//Basic data type 
+//{non-premitive type}
+//string 
+let firstName : string ="Mehedi";
+//number
+let roll :number =123;
+//boolean 
+let isAdmin :boolean =true;
+// undefine 
+let x :undefined =undefined;
+// null
+let y :null =null;
+
+let d :number;
+d=123;
+// d='add'
+
+
+// Array 
+let frineds : string[] =['mehedi','Hasan'];
+let eligableRollList : number[] =[1,2,3];
+// eligableRollList.push('aa');
+eligableRollList.push(4);
+
+// tuple >> array > order>type of values
+let coordinates :[number,number]=[1,5];
+let ageName :[number,string,boolean] =[50,'Mr.Mehedi',true]
+ageName[0]=23;
