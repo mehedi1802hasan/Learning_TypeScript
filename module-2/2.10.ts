09b49e558999c2f59e4bbb7b269703aaa89d15d0
mehedi1802hasan <mehedi1802hasan@gmail.com>
2024-05-06 16:53:03 +0600

    maping type  witg generics

---
+++ module-2/2.10.ts
@@ -0,0 +1,32 @@
+{
+    //
+     // mapped types>>
+     const arrOfNumbers : number[] = [1,2,3,4,5];
+     // const arrOfStrings : string[]=['1','2','3','4','5']
+     const arrOfStrings : string []= arrOfNumbers.map((number)=>number.toString())
+     console.log(arrOfStrings)
+
+
+ type AreaNumber ={
+    hight:number; 
+    width: number;
+ }
+
+ type AreaString ={
+    [index in keyof AreaNumber] : string;
+ }
+      
+    
+ type AreaMapingString <T> ={
+    [abc in keyof T] : T[abc];
+
+ };
+ const area1 : AreaMapingString <{
+    height: string; width: number
+ }>={
+    height:'100',
+    width:200
+ }
+    //
+
+}
