0bca6d7588ad21a72da34a15ee7b79a08ff64bc1
mehedi1802hasan <mehedi1802hasan@gmail.com>
2024-05-06 16:09:46 +0600

    conditaional type

---
+++ module-2/2.9.ts
@@ -0,0 +1,23 @@
+{
+    //
+// conditional type>>
+ 
+ type a1 = null;
+ type b1 = string;
+
+ type x =  a1 extends null ? true : false; // conditional type 
+ type y = a1 extends number ? true : b1 extends string ? string : any;
+
+
+ type Sheikh ={
+    bike : string ;
+    car : string;
+    ship: string;
+    plane: string;
+ }
+ 
+ type ChekhVehicle <T>= T extends keyof Sheikh ? true : false;
+ type HasVehicle = ChekhVehicle<'car'>
+
+    //
+}
